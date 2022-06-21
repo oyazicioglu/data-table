f358a110b8b064c7c1a34643819efb8947590d74
Ömer YAZICIOĞLU <oyazicioglu@hotmail.com>
2022-06-22 00:05:03 +0300

    Remove Cell, Row, Column definitions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-import Cell from './Cell.js';
-import Column from './Column.js';
-import Row from './Row.js';
 import DataTable from './DataTable.js';
 
-export { Row, Cell, Column, DataTable};
+export { DataTable };
